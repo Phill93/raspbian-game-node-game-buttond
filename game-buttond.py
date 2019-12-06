f18b8f10c1c04d3ecd066d65b7a9949c18af0d2a
Daniel Bacher <mail@phill93.de>
2019-12-06 09:31:56 +0100

    software ready for test

--- game-buttond.py
+++ game-buttond.py
@@ -2,6 +2,12 @@ from mqtt.mqtt import MQTT
 from board.board import Board
 import time
 import netifaces as ni
+import json
+import socket
+from button.led_button import LedButton
+
+led_button = LedButton(22, 18)
+
 
 def status_message():
     b = Board()
@@ -13,25 +19,60 @@ def status_message():
         "health": "not implemented",
         "validUntil": time.time() + 10
     }
+    return msg
 
 
-def button_message():
+def button_message(state, id="main"):
     msg = {
-        "id": "main",
-        "status": "pressed",
+        "id": id,
+        "status": state,
         "timestamp": time.time()
     }
+    return msg
 
 
-def led_message():
+def led_message(state, interval = False, color = False):
     msg = {
         "id": "main",
-        "state": "on",
-        "interval": 200,
-        "color": "CAFEE"
+        "state": state
     }
+    if interval:
+        msg["interval"] = interval
+    if color:
+        msg["color"] = color
+    return msg
 
-m = MQTT("mqtt.eclipse.org", [{"topic": "KIT/123", "callback": blubb}, {"topic": "KIT/#"}])
+
+def led_msg_handler(client: mqtt.Client, userdata, msg):
+    data = json.loads(msg.payload)
+    if data['state'] == "on":
+        led_button.led_on()
+    if data['state'] == "off":
+        led_button.led_off()
+    if data.['state'] == "blink":
+        if data['interval']:
+            led_button.led_blink_start(data['interval'])
+        else:
+            led_button.led_blink_start(200)
+
+topics = [
+    {
+        "topic": "buttongame/global/led",
+        "callback": led_msg_handler
+    },
+    {
+        "topic": "buttongame/buttons/" + socket.gethostname() + "/led",
+        "callback": led_msg_handler
+    }
+]
+
+m = MQTT("mqtt.eclipse.org", topics)
+m.mqttc.publish("buttongame/buttons/" + socket.gethostname() + "/led", led_message("blink"))
+
+def button_handler(channel):
+    m.mqttc.publish("buttongame/buttons/" + socket.gethostname() + "/button", button_message("pressed"))
+
+led_button.register_pressed_callback(button_handler)
 
 while True:
     time.sleep(1)
